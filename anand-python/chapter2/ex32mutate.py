02b7dd065fb986eead7afbf8ca4ccac9aa0e2194
shintocv <cshintov@gmail.com>
2015-03-25 15:53:32 +0530

    Added comments to ex32mutate.py

--- anand-python/chapter2/ex32mutate.py
+++ anand-python/chapter2/ex32mutate.py
@@ -1,5 +1,7 @@
 import sys
 import string
+
+#Inserts from [a-z] in each position of the string , returns the list of such words
 def insert(word,alpha):
   _list=[]
   for c in alpha:
@@ -8,17 +10,19 @@ def insert(word,alpha):
       mute_word.insert(pos,c)  
       _list.append(''.join(mute_word))
   return _list
-
+  
+#Removes the character from the word at the position i, returns the changed word
 def rem_char(word,i):
   word=list(word)
   word.remove(word[i])
   mute_word=''.join(word)
   return mute_word
-
+  
+#Deletes the character from each position, returns the list of such words
 def delete(word):
   _list=[rem_char(word,i) for i in range(len(word))]
   return _list
-
+#Replaces each character of the word by a char from [a-z], returns the list of such words
 def replace(word,alpha):
   _list=[]
   for c in alpha:
@@ -29,27 +33,25 @@ def replace(word,alpha):
       _list.append(''.join(mute_word))
   return _list
 
+#Given a position and the word, swaps the charaters at position and the next positions
 def swap(word,pos):
   word=list(word)
   word[pos],word[pos+1]=word[pos+1],word[pos]
   word=''.join(word)
   return word
 
+#Swaps adjacent characters of the word and returns the list of such words
 def swap_adj(word):
   _list=[swap(word,pos) for pos in range(len(word)-1)]
   return _list
   
-#mutates the word
+#mutates the word (insert with [a-z],delete a character,replace with [a-z],swap adjacent chars)
 def mutate(word):
   mute_list=[]
   alpha=string.lowercase
   mute_list.extend(insert(word,alpha))
-  #print mute_list
-  #print len(mute_list)'''
   mute_list.extend(delete(word))
-  #print mute_list
   mute_list.extend(replace(word,alpha))
-  #print mute_list
   mute_list.extend(swap_adj(word))  
   return mute_list
 
@@ -57,9 +59,9 @@ def mutate(word):
 
 def main():
   if len(sys.argv) ==2:
-    word=sys.argv[1] #command line arguments as ___list of strings
+    word=sys.argv[1] 
   else:
-    print "usage: python ex32mutate.py [alphabetic-word] "
+    print "usage: python ex32mutate.py 'string' "
     sys.exit(1)  
   result=mutate(word)
   print "The result : \n", result 
