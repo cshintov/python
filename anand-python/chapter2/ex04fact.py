ef75cda6cc1be1610b9f7cab04df25e5385d943d
shantocv <shantocv@gmail.com>
2015-03-26 17:59:18 +0530

    edited factorial

--- anand-python/chapter2/ex04fact.py
+++ anand-python/chapter2/ex04fact.py
@@ -2,19 +2,18 @@ import sys
 def prod(numlist):
   product=1
   for num in numlist:
-    product*=int(num)
+    product*=num
   return product
 def fact(num):
-  numb=int(num[0])
-  return prod(range(1,numb+1))
+    return prod(range(1,num+1))
 #This is the main function
 def main():
   if len(sys.argv) >=2:
-    args=sys.argv[1:]
+    num=int(sys.argv[1])
   else:
-    print "usage: python exn.py num "
+    print "usage: python ex04fact.py num "
     sys.exit(1)  
-  result=fact(args)
+  result=fact(num)
   print "factorial = ",result
 if __name__=="__main__":
   main()
