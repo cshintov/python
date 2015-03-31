7a72be2ade90cb03c0e803c3bcc11db4a08a0b02
shintocv <cshintov@gmail.com>
2015-03-31 09:30:41 +0530

    started chapter 5:iterators and generators

---
+++ anand-python/chapter5/ex01reverse-iter.py
@@ -0,0 +1,35 @@
+import sys
+class zrange:
+  def __init__(self,n):
+    self.value=n
+  def __iter__(self):
+    return reverse_iter(self.value)
+    
+class reverse_iter:
+  def __init__(self,n):
+    #print "In the iterator"
+    self.cur=n
+    self.val=0
+  def __iter__(self):
+    return self
+  def next(self):
+    if self.cur > self.val:
+      temp=self.cur
+      self.cur -= 1
+      return temp
+    else:
+      raise StopIteration()
+  
+
+#If both iteratable and iterator are the same object, it is consumed in a single iteration.
+print "Both iteratable and iterator are the same object"
+x=reverse_iter(int(sys.argv[1]))
+print list(x)
+print list(x) #will print empty list, since x consumed once
+
+#If iteratable and iterator are not the same object, the iterator object can be used many times
+print "Using different iterable and iterator"
+x=zrange(int(sys.argv[1]))
+print list(x)
+print list(x) #will print list as earlier , no empty list
+
